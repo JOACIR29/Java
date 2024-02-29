45c18677a9180807fabcb37825938358b85a517a
JOACIR VIANA <joacir.ines2014@gmail.com>
2024-02-28 22:17:20 -0400

    NumerosParesImpares

---
+++ Java/src/Execicio_56/NumerosParesImpares.java
@@ -0,0 +1,20 @@
+package Execicio_56;
+
+public class NumerosParesImpares {
+
+	public static void main(String[] args) {
+		System.out.println("Números Pares:");
+		for (int i = 1; i <= 50; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		System.out.println("\nNúmeros Ímpares:");
+		for (int i = 51; i <= 100; i++) {
+			if (i % 2 != 0) {
+			}
+		}
+	}
+
+}
