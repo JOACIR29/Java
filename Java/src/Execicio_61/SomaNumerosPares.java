a89e6114c21a037f70f9cbcb09adfa248cc9bfc8
JOACIR VIANA <joacir.ines2014@gmail.com>
2024-02-29 19:58:56 -0400

    SomaNumerosPares

---
+++ Java/src/Execicio_61/SomaNumerosPares.java
@@ -0,0 +1,15 @@
+package Execicio_61;
+
+public class SomaNumerosPares {
+
+	public static void main(String[] args) {
+		int soma = 0;
+		
+		for (int i = 2; i <= 100; i += 2) {
+			soma += i;
+		}
+		
+		System.out.println("A soma dos números pares de 1 a 100 é: " + soma);
+	}
+
+}
