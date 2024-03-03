efb4efc9bfc8c9ed1ae67ba85021273b84e24377
JOACIR VIANA <joacir.ines2014@gmail.com>
2024-03-03 00:40:31 -0400

    VerificarOrdemCrescente

---
+++ Java/src/Execicio_86/VerificarOrdemCrescente.java
@@ -0,0 +1,40 @@
+package Execicio_86;
+
+import java.util.Scanner;
+
+public class VerificarOrdemCrescente {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		
+		System.out.print("Digite o tamanho do vetor: ");
+		int tamanho = input.nextInt();
+		
+		int[] vetor = new int[tamanho];
+		
+		System.out.println("Digite os elementos do vetor:");
+		for (int i = 0; i < tamanho; i++) {
+			vetor[i] = input.nextInt();
+		}
+		
+		boolean estaEmOrdemCrescente = true;
+		
+		for (int i = 0; i < tamanho - 1; i++) {
+			if (vetor[i] > vetor[i + 1]) {
+				estaEmOrdemCrescente = false;
+				break;
+			}
+		}
+		
+		if (estaEmOrdemCrescente) {
+			System.out.println("O vetor está em ordem crescente.");
+		} else {
+			System.out.println("O vetor não está em ordem crescente.");
+		}
+		
+		input.close();
+
+	}
+
+}
