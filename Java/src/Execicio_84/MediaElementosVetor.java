c2c08a7d608869b03e508ae60273da003670a8e8
JOACIR VIANA <joacir.ines2014@gmail.com>
2024-03-02 23:54:35 -0400

    MediaElementosVetor

---
+++ Java/src/Execicio_84/MediaElementosVetor.java
@@ -0,0 +1,37 @@
+package Execicio_84;
+
+import java.util.Scanner;
+
+public class MediaElementosVetor {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Digite a quantidade de elementos: ");
+		int quantidade = input.nextInt();
+		
+		int[] vetor = new int[quantidade];
+		
+		System.out.println("Digite os elementos do vetor:");
+		
+		for (int i = 0; i < quantidade; i++) {
+			System.out.print("Elemento " + (i + 1) + ": ");
+			vetor[i] = input.nextInt();
+		}
+		
+		int soma = 0;
+		
+		for (int i = 0; i < quantidade; i++) {
+			soma += vetor[i];
+		}
+		
+		double media = (double) soma / quantidade;
+		
+		System.out.println("A média dos elementos do vetor é: " + media);
+		
+		
+		input.close();
+
+	}
+
+}
